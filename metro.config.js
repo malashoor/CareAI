42da8290ee84b7b3629ffbf285b91a2adfb217b8
Moayed <moayedalashoor@gmail.com>
2025-05-04 05:07:49 +0300

    Fix Metro bundling for Hermes with resolutions and add legacy peer deps config

---
+++ metro.config.js
@@ -0,0 +1,28 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Fix: Add support for SVGs and transpile node_modules/react-native
+defaultConfig.transformer = {
+  ...defaultConfig.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  minifierConfig: {
+    keep_classnames: true,
+    keep_fnames: true,
+    mangle: {
+      keep_classnames: true,
+      keep_fnames: true,
+    },
+  },
+};
+
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg');
+defaultConfig.resolver.sourceExts.push('svg');
+
+// Enable export mapping and transpilation of react-native
+defaultConfig.resolver.unstable_enablePackageExports = true;
+defaultConfig.resolver.extraNodeModules = {
+  'react-native': require.resolve('react-native'),
+};
+
+module.exports = defaultConfig; 
