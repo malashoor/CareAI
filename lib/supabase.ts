1a2f1e859de7ac7244dc76301b8baeeb68033094
avnishynr <avnish80.choudhary@gmail.com>
2025-06-11 10:22:00 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://wpzpmgvqcanvtjusxbeg.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwenBtZ3ZxY2FudnRqdXN4YmVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1NDgyNjksImV4cCI6MjA1ODEyNDI2OX0.oMrRv28hCYUydx-t4Hn-zk9Tv5k71xG81XP8jhZfguo'
-
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables. Please check your .env file.');
